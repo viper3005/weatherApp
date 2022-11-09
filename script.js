d4e0ae5f475ccacab4592524ac940789ace68b80
viper3005 <105823608+viper3005@users.noreply.github.com>
2022-11-09 23:19:24 +0100

    add the first version of the app

---
+++ script.js
@@ -0,0 +1,39 @@
+let weather = {
+    apiKey: "eebf8dd0c2f688b13b0b87038e53274e",
+    checkWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city +
+            "&units=metric&appid="
+            + this.apiKey + "")
+            .then((response) => response.json())
+            .then((data) => this.dysplayWeather(data));
+    },
+    dysplayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".temp").innerText = Math.floor(temp) + " °C";
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + " %";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1200/?" + name + "')";
+    },
+    search: function(){
+       this.checkWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", function(){
+weather.search();
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if (event.key === "Enter"){
+        weather.search();
+    }
+})
